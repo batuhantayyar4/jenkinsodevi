3ed1aa1fc87179368fe908eb623211b11f795e73
batuhantayyar4 <158208406+batuhantayyar4@users.noreply.github.com>
2024-02-01 19:54:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,28 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
-import pandas as pd
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-api = Api(app)
 
-class Yolcu(Resource):
-    def get(self):
-        data = pd.read_csv('yolcular.csv')
-        data = data.to_dict('records')
-        return {'data': data}, 200
+def caesar_cipher(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isalpha():
+            shift_amount = shift % 26
+            if char.isupper():
+                result += chr((ord(char) + shift_amount - 65) % 26 + 65)
+            else:
+                result += chr((ord(char) + shift_amount - 97) % 26 + 97)
+        else:
+            result += char
+    return result
 
-    def post(self):
-        isim = request.args['isim']
-        soyisim = request.args['soyisim']
-        kalkisYeri = request.args['kalkisYeri']
-        varisYeri = request.args['varisYeri']
-        kalkisSaati = request.args['kalkisSaati']
-        varisSaati = request.args['varisSaati']
-        koltukNo = request.args['koltukNo']
-        aracNo = request.args['aracNo']
-        plaka = request.args['plaka']
-        ucret = request.args['ucret']
-
-        data = pd.read_csv('yolcular.csv')
-
-        new_data = pd.DataFrame({
-            'isim': [isim],
-            'soyisim': [soyisim],
-            'kalkisYeri': [kalkisYeri],
-            'varisYeri': [varisYeri],
-            'kalkisSaati': [kalkisSaati],
-            'varisSaati': [varisSaati],
-            'koltukNo': [koltukNo],
-            'aracNo': [aracNo],
-            'plaka': [plaka],
-            'ucret': [ucret]
-        })
-
-        data = data.append(new_data, ignore_index=True)
-        data.to_csv('yolcular.csv', index=False)
-        return {'data': new_data.to_dict('records')}, 200
-
-class Arac(Resource):
-    def get(self):
-        data = pd.read_csv('yolcular.csv', usecols=[7])
-        data = data.to_dict('records')
-        return {'data': data}, 200
-
-class YolcuAra(Resource):
-    def get(self, isim):
-        data = pd.read_csv('yolcular.csv')
-        data = data.to_dict('records')
-        for entry in data:
-            if entry['isim'] == isim:
-                return {'data': entry}, 200
-        return {'message': 'No entry found with this name !'}, 404
-
-api.add_resource(Yolcu, '/yolcular', methods=['GET', 'POST'])
-api.add_resource(Arac, '/araclar')
-api.add_resource(YolcuAra, '/<string:isim>')
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    data = request.get_json()
+    text = data['text']
+    shift = data['shift']
+    encrypted_text = caesar_cipher(text, shift)
+    return jsonify({"encrypted_text": encrypted_text})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
